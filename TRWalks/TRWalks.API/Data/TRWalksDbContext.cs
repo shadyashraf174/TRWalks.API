cfb01791082b51b7776b42091e9647c0be0cfe30
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2024-12-08 23:33:28 +0700

    Creating the DBContext Class

---
+++ TRWalks/TRWalks.API/Data/TRWalksDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TRWalks.API.Models.Domain;
+
+namespace TRWalks.API.Data {
+
+
+    public class TRWalksDbContext: DbContext {
+
+        public TRWalksDbContext(DbContextOptions dbContextOptions) :base(dbContextOptions) 
+        {
+            
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> walks { get; set; }
+
+    }
+}
