04f1a00e566f6df69e8f524284a12a6c8061d2f2
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2024-12-08 18:34:54 +0700

    Running and Calling ASP.NET Core Web API

---
+++ TRWalks/TRWalks.API/Controllers/studentsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TRWalks.API.Controllers {
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class studentsController : ControllerBase {
+
+        [HttpGet]
+        public IActionResult getAllStudents() {
+            String[] studentsName = new String[] { "shady", "Ashraf", "Alaa" };
+            return Ok(studentsName);    
+        }
+    }
+}
