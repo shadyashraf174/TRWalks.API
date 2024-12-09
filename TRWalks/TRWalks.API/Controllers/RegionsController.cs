bdc12d855c3a905512e240708c89f689a866d5e5
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2024-12-09 17:34:52 +0700

    Update Region Endpoint - PUT

--- TRWalks/TRWalks.API/Controllers/RegionsController.cs
+++ TRWalks/TRWalks.API/Controllers/RegionsController.cs
@@ -79,5 +79,30 @@ namespace TRWalks.API.Controllers {
             // Return a 201 response with location
             return CreatedAtAction(nameof(GetById), new { id = regionDto.Id }, regionDto);
         }
+
+        // Update region
+        // PUT: https://localhost:portnumber/api/regions/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto) {
+            // Check if region exists
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomainModel == null) {
+                return NotFound();
+            }
+            // Map DTO to Domain model
+            regionDomainModel.Code = updateRegionRequestDto.Code;
+            regionDomainModel.Name = updateRegionRequestDto.Name;
+            regionDomainModel.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+            dbContext.SaveChanges();
+            // Convert Domain Model to DTO
+            var regionDto = new RegionDTO {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
     }
 }
---
+++ TRWalks/TRWalks.API/DTO/UpdateRegionRequestDto.cs
@@ -0,0 +1,9 @@
+﻿namespace TRWalks.API.DTO {
+    public class UpdateRegionRequestDto {
+        public string Code { get; set; }
+
+        public string Name { get; set; }
+
+        public string? RegionImageUrl { get; set; }
+    }
+}
