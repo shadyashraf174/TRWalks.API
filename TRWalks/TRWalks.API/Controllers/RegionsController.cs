c4931ee2c620a0ec1d70d8358f83f1b5a2db9cc5
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2024-12-09 17:50:35 +0700

    Add Delete Region Endpoint

--- TRWalks/TRWalks.API/Controllers/RegionsController.cs
+++ TRWalks/TRWalks.API/Controllers/RegionsController.cs
@@ -104,5 +104,28 @@ namespace TRWalks.API.Controllers {
             };
             return Ok(regionDto);
         }
+
+        // Delete Region
+        // DELETE: https://localhost:portnumber/api/regions/{id}
+        [HttpDelete]
+        [Route("{id:Guid}")]
+        public IActionResult Delete([FromRoute] Guid id) {
+            var regionDomainModel = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (regionDomainModel == null) {
+                return NotFound();
+            }
+            // Delete region
+            dbContext.Regions.Remove(regionDomainModel);
+            dbContext.SaveChanges();
+            // return deleted Region back
+            // map Domain Model to DTO
+            var regionDto = new RegionDTO {
+                Id = regionDomainModel.Id,
+                Code = regionDomainModel.Code,
+                Name = regionDomainModel.Name,
+                RegionImageUrl = regionDomainModel.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
     }
 }
