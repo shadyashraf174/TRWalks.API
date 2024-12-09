4ef0386de4f35039b0e2336d61dcada850a6df05
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2024-12-09 15:17:03 +0700

    Implement "Get Region by ID" Endpoint

--- TRWalks/TRWalks.API/Controllers/RegionsController.cs
+++ TRWalks/TRWalks.API/Controllers/RegionsController.cs
@@ -9,16 +9,26 @@ namespace TRWalks.API.Controllers {
     public class RegionsController : ControllerBase {
         private readonly TRWalksDbContext dbContext;
 
-        public RegionsController(TRWalksDbContext dbContext)
-        {
+        public RegionsController(TRWalksDbContext dbContext) {
             this.dbContext = dbContext;
         }
 
         [HttpGet]
         public IActionResult GetAll() {
-            
+
             var regions = dbContext.Regions.ToList();
 
+            return Ok(regions);
+        }
+
+        [HttpGet]
+        [Route("{id:Guid}")]
+        public IActionResult GetById([FromRoute] Guid id) {
+            var regions = dbContext.Regions.Find(id);
+
+            if (regions == null) {
+                return NotFound();
+            }
             return Ok(regions);
         }
     }
