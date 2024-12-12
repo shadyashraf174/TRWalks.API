ca19888a87dc4fa0f1f79fdbdae4e4b4f941eab1
Shady Ashraf Abdelhameed <shadyashraf174@gmail.com>
2024-12-12 22:52:03 +0700

    Setting Up Authentication and Testing it

--- TRWalks/TRWalks.API/Controllers/RegionsController.cs
+++ TRWalks/TRWalks.API/Controllers/RegionsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -11,6 +12,7 @@ using TRWalks.API.Repositories;
 namespace TRWalks.API.Controllers {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class RegionsController : ControllerBase {
         private readonly TRWalksDbContext dbContext;
         private readonly IRegionRepository regionRepository;
